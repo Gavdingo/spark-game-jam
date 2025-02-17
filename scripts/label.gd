dcf3a9c428d9b9e72b52b6b9462b96ebaf94814d
Gavdingo <gl261940@gmail.com>
2025-02-17 12:44:56 -0700

    added title, minor fixes

---
+++ scripts/label.gd
@@ -0,0 +1,12 @@
+extends Label
+
+var label : Label
+
+@export var score = 0
+
+func _ready() -> void:
+	label = get_node("../Label")
+
+
+func _process(delta: float) -> void:
+	label.text = "score: " + str(score ** 2)
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,7 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://fst.tscn")
+	get_tree().change_scene_to_file("res://scenes/fst.tscn")
 
 
 func _on_option_button_pressed() -> void:
